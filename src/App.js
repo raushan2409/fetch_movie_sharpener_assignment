cbec40695b16027a23f5253a7ff5b561de272a2f
Raushan <roushankumarsingh2018@gmail.com>
2023-11-09 19:55:15 +0530

    automatically loading the page with useEffect and useCallback

--- src/App.js
+++ src/App.js
@@ -1,29 +1,31 @@
-import React, { useState } from "react";
+import React, { useEffect, useState,useCallback } from "react";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
 
-// async await means async before function and it will wait till ur done or operation
-
 function App() {
   const [movies, setMovies] = useState([]);
-  // for data spinner till we didn't get the data  we can use state
 
   const [isLoading, setIsLoading] = useState(false);
 
-  const [error, setError] = useState(null); //in case of .then .catch we can directly catch the error so here we use try catch  for handling error. now one problem we can face here is the fetch api doesn't treat error status codes as real error it will not throw an technical error if we get back an error status code ,it would be better if we throw a real error if we get the response for unsuccessfull status code and by default fetch doesn't have axios have by default . so we have to do this manually so in that case we have to do this manually by response.ok field (response is succesful or not ) the response also has a staus field which holds the concrete response status code. u could also manually check that.
+  const [error, setError] = useState(null);
 
-  async function fetchMoviesHandler() {
+  // this will also run
+  // useEffect(()=>{
+  //   fetchMoviesHandler();
+  // },[])
+  
+  
+
+  const fetchMoviesHandler = useCallback(async function() {
     setIsLoading(true);
     setError(null);
 
     try {
-      const response = await fetch(`https://swapi.dev/api/film/`);
+      const response = await fetch(`https://swapi.dev/api/films/`);
       if (!response.ok) {
-        // throw new Error("Something went wrong!"); //u can pass ur error msg
         throw new Error("....Retrying"); //u can pass ur error msg
       }
-      // in some case u have to pass response.ok before response.json some api also send the json data if req was not successful other api like this one sents the non json data so we have to write before parse that
       const data = await response.json();
 
       // console.log("data",data)
@@ -35,13 +37,18 @@ function App() {
           releaseDate: movieData.release_date,
         };
       });
-      // store the results in some state
+
       setMovies(transformedMovies);
     } catch (error) {
       setError(error.message);
     }
     setIsLoading(false);
-  }
+    // console.log(isLoading);
+  },[])
+  useEffect(()=>{
+    fetchMoviesHandler();
+  },[fetchMoviesHandler])
+
 
   return (
     <React.Fragment>
@@ -54,9 +61,9 @@ function App() {
         {isLoading && <p>Loading...</p>}
         {!isLoading && error && <p>{error}</p>}
 
-        {!isLoading && error && (
+        {/* {!isLoading && error && (
           <button onClick={() => setError(null)}>Cancel Retry</button>
-        )}
+        )} */}
       </section>
     </React.Fragment>
   );
